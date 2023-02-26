924bd387c3138114e0d2a5daa6128226d24036bf
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-02-27 01:10:34 +0600

    changes in pushing the item

--- src/components/redux/addCart/actionTypes.js
+++ src/components/redux/addCart/actionTypes.js
@@ -1,6 +1,6 @@
 export const ADDPRODUCT = "addProduct"
-
-// testpurpos
 export const ADDTOCART = "addTocart"
 export const INCREMENTITEM = "incrementitem"
 export const DECREMENTITEM = "decrementitem"
+
+// tp
