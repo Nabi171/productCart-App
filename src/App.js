2ebb1294b26b6dcc8ca314bff0a8850213ec5f35
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-02-27 18:28:16 +0600

    change in code

--- src/App.js
+++ src/App.js
@@ -28,8 +28,30 @@ function App() {
     <Provider store={store}>
       <div >
         {/* <!-- Navbar --> */}
-        <Header />
+        <Header
+          handleClick={handleClick}
+        />
+        {/* <nav className="bg-[#171C2A] py-4">
+          <div className="navBar">
+            <a href="index.html"
+              onClick={handleClick}
+            >
+              <img src="./images/logo.png" alt="LWS" className="max-w-[140px]" />
+            </a>
 
+            <div className="flex gap-4">
+              <a href="#home" className="navHome" id="lws-home"
+                onClick={handleClick}
+              > Home </a>
+              <a href="cart.html" className="navCart" id="lws-cart"
+                onClick={handleClick}
+              >
+                <i className="text-xl fa-sharp fa-solid fa-bag-shopping"></i>
+                <span id="lws-totalCart">0</span>
+              </a>
+            </div>
+          </div>
+        </nav> */}
         {/* <!-- Navbar ends --> */}
         {showCartComponent ? <ComponentAddtoCart /> : <ComponentCartItem />}
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-const Header = () => {
+const Header = ({ handleClick }) => {
     const cartItems = useSelector((state) => state.cartItems);
     return (
         <div>
             <nav className="bg-[#171C2A] py-4">
                 <div className="navBar">
-                    <a href="index.html">
+                    <a href="index.html"
+                        onClick={handleClick}
+                    >
                         <img src="./images/logo.png" alt="LWS" className="max-w-[140px]" />
                     </a>
 
                     <div className="flex gap-4">
-                        <a href="#home" className="navHome" id="lws-home"> Home </a>
-                        <a href="cart.html" className="navCart" id="lws-cart">
+                        <a href="#home" className="navHome" id="lws-home"
+                            onClick={handleClick}
+                        > Home </a>
+                        <a href="cart.html" className="navCart" id="lws-cart"
+                            onClick={handleClick}
+                        >
                             <i className="text-xl fa-sharp fa-solid fa-bag-shopping"></i>
                             <span id="lws-totalCart">{cartItems.length}</span>
                         </a>
