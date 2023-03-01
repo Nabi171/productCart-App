a192a414d083cad6f97365147906f05e9313e3b5
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-03-02 00:03:17 +0600

    as

--- src/components/redux/addCart/actionTypes.js
+++ src/components/redux/addCart/actionTypes.js
@@ -2,9 +2,6 @@ export const ADDPRODUCT = "addProduct"
 export const ADDTOCART = "addTocart"
 export const INCREMENTITEM = "incrementitem"
 export const DECREMENTITEM = "decrementitem"
-
-// tp
-
 export const ADDTOTATAL = "addTotal"
 
 export const DELETE_PRODUCT = "deleteProduct";
